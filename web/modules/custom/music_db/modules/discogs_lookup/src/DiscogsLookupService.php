48a1aec263a6f91b58d67dfb6c563278e73ba08d
jinkiesz <larak22@ru.is>
2025-12-09 16:16:07 +0000

    added discogs search service

---
+++ web/modules/custom/music_db/modules/discogs_lookup/src/DiscogsLookupService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\discogs_lookup;
+
+use Drupal\Core\Config\Config;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+
+class DiscogsLookupService
+{
+  protected ClientInterface $httpClient;
+  protected ConfigFactoryInterface $configFactory;
+
+  protected Config $config;
+  public function __construct(ClientInterface $http_client, protected ConfigFactoryInterface $config_factory)
+  {
+    $this->httpClient = $http_client;
+    $this->configFactory = $config_factory;
+    $this->config = $this->configFactory->get('discogs_lookup.settings');
+
+  }
+
+  public function search(string $query, string $type = 'track', ?int $limit = NULL): array
+  {
+    $baseUrl = $this->config->get('api_uri') ?: 'https://api.discogs.com/database/search';
+    $limit ??= (int)($this->config->get('max_hits') ?? 20);
+
+    $token = $this->config->get('token');
+
+    $headers = [
+      'User-Agent' => 'MusicDBDrupal +https://tonlistar-skraning.ddev.site',
+    ];
+
+    $queryParams = [
+      'q' => $query,
+      'type' => $type,
+      'per_page' => $limit,
+      'page' => 1,
+    ];
+
+    if ($token) {
+      $headers['Authorization'] = 'Discogs token=' . trim($token);
+    }
+
+    $response = $this->httpClient->get($baseUrl, [
+      'headers' => $headers,
+      'query' => $queryParams,
+    ]);
+
+    $data = json_decode($response->getBody()->getContents(), TRUE);
+
+    return $data;
+  }
+}
+
