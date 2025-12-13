a5bbe99619f5bdecd615656d7333cf150aebdd29
Poomi777 <pkm.poomi@gmail.com>
2025-12-13 15:48:27 +0000

    added the menu navigations for the custom entities
    tried to make a glossary appear for the custom artist entity page through php, but only managed to make it appear for the glossary pages (aka /custom-artists/m or etc other letters)

--- web/modules/custom/music_db/src/Controller/ArtistListController.php
+++ web/modules/custom/music_db/src/Controller/ArtistListController.php
@@ -6,6 +6,8 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+use Drupal\Core\Url;
+
 final class ArtistListController extends ControllerBase {
 
   /**
@@ -35,18 +37,44 @@ final class ArtistListController extends ControllerBase {
   }
 
   /**
-   * Page callback: list artists.
+   * @param string|null $letter
+   *   Optional A-Z letter filter.
    *
    * @return array
    *   A render array.
    */
-  public function list() {
+  public function list($letter = NULL) {
+    $active_letter = $letter ? strtoupper($letter) : NULL;
+
+    $links = [];
+    $links['all'] = [
+      'title' => $this->t('All'),
+      'url' => Url::fromRoute('music_db.custom_artists'),
+      'attributes' => [
+        'class' => !$active_letter ? ['is-active'] : [],
+      ],
+    ];
+
+    foreach (range('A', 'Z') as $char) {
+      $links[$char] = [
+        'title' => $char,
+        'url' => Url::fromRoute('music_db.custom_artists_letter', ['letter' => $char]),
+        'attributes' => [
+          'class' => ($active_letter === $char) ? ['is-active'] : [],
+        ],
+      ];
+    }
+
     $storage = $this->entityTypeManager->getStorage('artist');
+    $query = $storage->getQuery()->accessCheck(TRUE);
 
-    $query = $storage->getQuery()
-      ->accessCheck(TRUE);
+    $label_field = 'name'; 
 
-    $query->sort('name', 'ASC');
+    if ($active_letter) {
+      $query->condition($label_field, $active_letter, 'STARTS_WITH');
+    }
+
+    $query->sort($label_field, 'ASC');
 
     $ids = $query->execute();
     $artists = $ids ? $storage->loadMultiple($ids) : [];
@@ -57,13 +85,24 @@ final class ArtistListController extends ControllerBase {
     }
 
     return [
-      '#theme' => 'item_list',
-      '#items' => $items,
-      '#empty' => $this->t('No artists found.'),
+      'glossary' => [
+        '#theme' => 'links',
+        '#links' => $links,
+        '#attributes' => ['class' => ['artist-glossary']],
+      ],
+      'list' => [
+        '#theme' => 'item_list',
+        '#items' => $items,
+        '#empty' => $active_letter
+          ? $this->t('No artists found starting with @letter.', ['@letter' => $active_letter])
+          : $this->t('No artists found.'),
+      ],
       '#cache' => [
         'tags' => $this->entityTypeManager->getDefinition('artist')->getListCacheTags(),
+        'contexts' => ['url.path'],
       ],
     ];
   }
+
 
 }
